c99a579440b04278aad13a9c5e7ad32ff185391f
Flvftyy <161535962+ElegantIsBlatant@users.noreply.github.com>
2026-04-14 12:37:59 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
-  challenge: tru3, // Set to true if you want to enable password protection.
+  challenge: true, // Set to true if you want to enable password protection.
   users: {
     // You can add multiple users by doing username: 'password'.
     Me: "1",
